a3e776f0a34a6f61fc03ac1db777c7e2550bced3
Alexander <Alexander>
2016-07-22 12:44:41 +0300

    ic

---
+++ UnityTest2/Assets/Scene Menu/UICallbacks.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UICallbacks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void on360click()
+    {
+        SceneManager.LoadScene("Scene360Video");
+    }
+
+    public void onVRclick()
+    {
+        SceneManager.LoadScene("SceneVR");
+    }
+
+    public void onARclick()
+    {
+        SceneManager.LoadScene("SceneAR");
+    }
+
+    public void onBackClick()
+    {
+        SceneManager.LoadScene("SceneMenu");
+    }
+}

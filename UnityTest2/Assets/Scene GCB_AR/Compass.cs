00783b7da44dda061692a5321c7bd28bd95cd601
Alexander <Alexander>
2016-07-27 16:59:29 +0300

    last scene

--- UnityTest2/Assets/Scene GCB_AR/Compass.cs
+++ UnityTest2/Assets/Scene GCB_AR/Compass.cs
@@ -3,53 +3,73 @@ using System.Collections;
 using UnityEngine.UI;
 using System;
 
-public class Compass : MonoBehaviour {
+public class Compass : MonoBehaviour
+{
 
     GameObject center;
     GameObject water_label;
     GameObject arrow;
-
     GameObject toggle;
+    GameObject compass;
 
-	// Use this for initialization
-	void Start () {
+    float compassAngle = 0;
+
+    // Use this for initialization
+    void Start()
+    {
         Input.location.Start();
         Input.compass.enabled = true;
 
         center = GameObject.Find("Center");
+        toggle = GameObject.Find("Toggle");
+        compass = GameObject.Find("ImageCompass");
+
         water_label = GameObject.Find("Water label");
         arrow = GameObject.Find("Arrow");
-        toggle = GameObject.Find("Toggle");
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
+        // Use real compass
         if (toggle.GetComponent<Toggle>().isOn)
         {
-            float compassAngle = Input.compass.trueHeading;
-
-            Vector3 projection = transform.forward - Vector3.Project(transform.forward, Vector3.up);
-            float cameraAngle = Mathf.Rad2Deg * Mathf.Atan2(projection.x, projection.z);
-
-
-            float delta = compassAngle - cameraAngle;
-            center.transform.rotation = Quaternion.Slerp(center.transform.rotation, Quaternion.Euler(0, center.transform.rotation.eulerAngles.y + delta, 0), Time.deltaTime / 2f);
-
-
-            string message = String.Format("Hola! Compass: {0}, camera: {1}.", compassAngle, cameraAngle);
-            GameObject.Find("Message text").GetComponent<Text>().text = message;
+            compassAngle = Input.compass.trueHeading;
+        }
+        else
+        {
+            // Read compass from controller
+            if (Input.touchCount == 1)
+            {    
+                compassAngle += -Input.GetTouch(0).deltaPosition.x;
+            }
         }
 
+        GameObject camera = GameObject.Find("Camera");
+        Vector3 projection = camera.transform.forward - Vector3.Project(camera.transform.forward, Vector3.up);
+        float cameraAngle = Mathf.Rad2Deg * Mathf.Atan2(projection.x, projection.z);
 
-        // Swipes
+
+        float delta = compassAngle - cameraAngle;
+        //center.transform.rotation = Quaternion.Slerp(center.transform.rotation, Quaternion.Euler(0, center.transform.rotation.eulerAngles.y + delta, 0), Time.deltaTime / 2f);
+        center.transform.rotation = Quaternion.Slerp(center.transform.rotation, Quaternion.Euler(0, center.transform.rotation.eulerAngles.y + delta, 0), 0.1f);
+
+
+        string message = String.Format("Compass: {0}, camera: {1}.", compassAngle, cameraAngle);
+        GameObject.Find("Message text").GetComponent<Text>().text = message;
+
+
+        // Arrow height
         if (Input.touchCount == 1)
         {
-            center.transform.Rotate(0, -Input.GetTouch(0).deltaPosition.x, 0);
             water_label.transform.Translate(0, Input.GetTouch(0).deltaPosition.y, 0, Space.World);
         }
 
         // Arrow rotate
         arrow.transform.Rotate(0, -30 * Time.deltaTime, 0, Space.World);
+
+        // Compass
+        compass.transform.rotation = Quaternion.Euler(0, 0, compassAngle);
     }
 }

1b899767bd4a31a78c8cb761ec1e7666901d501b
Alexander <Alexander>
2016-08-02 16:46:38 +0300

    something was added

---
+++ UnityTest2/Assets/Scene Menu/MyMenuItem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyMenuItem : MonoBehaviour
+{
+    private Vector3 BaseScale;
+    private float HighlightScale = 1.2f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        BaseScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isPointerHover())
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, BaseScale * HighlightScale, Time.deltaTime * 2.5f);
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, BaseScale, Time.deltaTime * 2.5f);
+        }
+	}
+
+    bool isPointerHover()
+    {
+        RaycastHit hit;
+        GameObject camera = GameObject.Find("Main Camera");
+
+        bool pointerHover = false;
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 100))
+        {
+            if (hit.transform.gameObject == transform.gameObject)
+            {
+                pointerHover = true;
+            }
+        }
+
+        return pointerHover;
+    }
+}
---
+++ UnityTest2/Assets/Scene Menu/ViewStabilizer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewStabilizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Calculate camera direction
+        GameObject camera = GameObject.Find("Main Camera");
+        Vector3 projection = camera.transform.forward - Vector3.Project(camera.transform.forward, Vector3.up);
+        float cameraAngle = Mathf.Rad2Deg * Mathf.Atan2(projection.x, projection.z);
+
+        float aim = cameraAngle;
+        float bound = 50;
+
+        if (aim > bound)
+        {
+            aim = bound;
+        }
+
+        if (aim < -bound)
+        {
+            aim = -bound;
+        }
+
+        Quaternion destination = Quaternion.Euler(0, transform.rotation.eulerAngles.y - cameraAngle + aim, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, destination, 1f * Time.deltaTime);
+    }
+}
